0b254c7017ec860be9a7be7f2307040e484562a2
Luis Chavez <wally@Luiss-MacBook-Pro.local>
2022-12-08 20:01:51 -0600

    moved sentiment logic from main script to respective module and further commented code

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 # Author: Luis Wally Chavez Quiroz
 # Email: lwchave2@illinois.edu
-# Date: 12.6.2022
+# Date: 12.8.2022
 # Description: Script for running MLTK 
 
 from twitterDocumentHarvester import TwitterDocumentHarvester
+from sentiment import Sentiment
 
 import sys
 import os
 import shutil
 import subprocess
 from datetime import datetime
-
-
-#temp holder for sentiment
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
 import glob
-#temp holder for sentiment
 
 
 directory_results = "results/"
@@ -25,6 +19,7 @@ str_date_time = ""
 platform = ''
 media_item = ''
 
+# logs the date and time of query for use in output documents see /results folder
 def time_log():
     current_time = datetime.now()
     time_stamp = current_time.timestamp()
@@ -32,6 +27,7 @@ def time_log():
     str_date_time = date_time.strftime("%d-%m-%Y_%H-%M-%S")
     return str_date_time
 
+# cleans up /results directory and initializes time to log
 def initialize_results():
     global str_date_time
     if (os.path.exists(directory_results)):
@@ -39,6 +35,7 @@ def initialize_results():
     os.makedirs(directory_results)
     str_date_time = time_log()
 
+# prompts user for (1) social media platform AND (2) media item query 
 def sentiment_request():
     greeting_message = "Welcome to MLTK, a social media sentiment analysis tool for media (books, movies, games, etc). Please provide a social media platform and media item."
     print(greeting_message)
@@ -47,6 +44,7 @@ def sentiment_request():
     sentiment_query = [platform_in, media_item_in]
     return sentiment_query
 
+# retrieves documents of specified social media platform
 def harvest_documents():
     global platform, media_item
     sentiment_query = sentiment_request()
@@ -64,12 +62,13 @@ def harvest_documents():
     results_file = directory_results + prefix + media_item + "---" + str_date_time + ".txt"
     harvester.generate_file(results_file)
 
+# ranks documents via sub-process
 def rank_documents():
     # run through scripted shell to allow for sub-process virtual environment 
     print("Ranking Twitter documents...")
-    subprocess.run(["/Users/wally/Documents/School/Courses/FA2022/CS410/MLTK/rank.sh"], shell=True)
+    subprocess.run(["./rank.sh"], shell=True)
 
-# TEMP place holder for SENTIMENT segment
+# peforms sentiment analysis of documents
 def analyze_documents():
     ranked_file = ''
     for file in glob.glob(directory_results+"*.txt"):
@@ -77,57 +76,20 @@ def analyze_documents():
             ranked_file = file
 
     analyzed_file = ranked_file
-    analyzed_file = analyzed_file.replace(directory_results + "ranked_", directory_results + "analyzed_")
-
-    with open(ranked_file, "r") as rf, open(analyzed_file, "w") as wf:
-        ranked_file_lines = rf.readlines()
-        counter = 0
-        for line in ranked_file_lines:
-            processed_line = line.split(" *** ")
-            clean_line = processed_line[1]
-            sid = SentimentIntensityAnalyzer()
-            ss = sid.polarity_scores(clean_line)
-            to_write = ''
-            for k in sorted(ss):
-                to_write += '{0}: {1}, '.format(k, ss[k])
-            to_write = to_write + "\n"
-            wf.write(to_write)
-            counter += 1
-
-def report_results():
-    analyzed_file = ''
-    for file in glob.glob(directory_results+"*.txt"):
-        if "analyzed_" in file:
-            analyzed_file = file
-
     scored_file = analyzed_file
+
+    analyzed_file = analyzed_file.replace(directory_results + "ranked_", directory_results + "analyzed_")
     scored_file = analyzed_file.replace(directory_results + "analyzed_", directory_results + "scored_")
 
-    with open(analyzed_file, "r") as rf, open(scored_file, "w") as wf:
-        analyzed_file_lines = rf.readlines()
-        rating_list = []
-        for line in analyzed_file_lines:
-            ratings = line.replace('compound: ', '')
-            ratings_split = ratings.split(', ')
-            rating_list.append(float(ratings_split[0]))
-        avg_rating = sum(rating_list) / len(rating_list)
-
-        result = normalize([avg_rating], {'actual': {'lower': -1, 'upper': 1}, 'desired': {'lower': 0, 'upper': 5}})
-        to_write = "average rating = {0} range:(-1, 1), normalized rating = {1} range:(0, 5)".format(avg_rating, result)
-        wf.write(to_write)
-
-        print("{0} has a favoritibility of {1:.2f}/5 on {2}".format(media_item, result[0], platform))
-
-def normalize(values, bounds):
-    return [bounds['desired']['lower'] + (x - bounds['actual']['lower']) * (bounds['desired']['upper'] - bounds['desired']['lower']) / (bounds['actual']['upper'] - bounds['actual']['lower']) for x in values]
-
-# TEMP place holder for SENTIMENT segment
+    analyzer = Sentiment()
+    analyzer.generate_analyzed_file(ranked_file, analyzed_file)
+    result = analyzer.generate_scored_file(analyzed_file, scored_file)
+    print("{0} has a favoritibility of {1:.2f}/5 on {2}".format(media_item, result, platform))
 
 def main():
     harvest_documents()
     rank_documents()
     analyze_documents()
-    report_results()
 
 if __name__ == "__main__":
     main()
--- sentiment/sentimentanalyzer.py
+++ sentiment/sentimentanalyzer.py
@@ -1,18 +1,59 @@
+# Author: Luis Wally Chavez Quiroz
+# Email: lwchave2@illinois.edu
+# Date: 12.8.2022
+# Description: Sentiment module
+
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+
 class Sentiment:
     # Constructor method of Sentiment
-    def __init__(self, tempInput):
-        self.tempInput = tempInput
-        self.tempInternalField = None
-        self.foo() # set internal field to whatever it needs to be via foo()
-
+    def __init__(self):
+        self.rankedFile = None
+        self.analyzedFile = None
+        self.scoredFile = None
     
     # implement Vader out-of-box sentiment analyzer then try training or hardcoding emojis for positive
-    def foo(self):
-        pass
+    def generate_analyzed_file(self, rankedFile, analyzedFile):
+        with open(rankedFile, "r") as rf, open(analyzedFile, "w") as wf:
+            ranked_file_lines = rf.readlines()
+            for line in ranked_file_lines:
+                processed_line = line.split(" *** ")
+                clean_line = processed_line[1]
+                sid = SentimentIntensityAnalyzer()
+                ss = sid.polarity_scores(clean_line)
+                to_write = ''
+                for k in sorted(ss):
+                    to_write += '{0}: {1}, '.format(k, ss[k])
+                to_write = to_write + "\n"
+                wf.write(to_write)
 
-    
-    
+    # trasforming rating rating range from [-1, 1] to any range [1, 5] by default in 'generate_scored_file' section
+    def normalize(self, sourceLowerBound, sourceUpperBound, destinationLowerBound, destinationUpperBound, sourceValue):
+        deltaLower = destinationLowerBound - sourceLowerBound
+        rangeSource = sourceUpperBound - sourceLowerBound
+        rangeDestination = destinationUpperBound - destinationLowerBound
+        magnification = rangeDestination / rangeSource
+        translatedX = sourceValue + deltaLower
+        start = destinationLowerBound - magnification * (sourceLowerBound + deltaLower)
+        destinationValue = magnification * translatedX + start
 
+        return destinationValue
+
+    # generate overall sentiment score of user query using a [1, 5] range (typical for Google Rating, Apple Store, etc)
+    def generate_scored_file(self, analyzedFile, scoredFile):
+        with open(analyzedFile, "r") as rf, open(scoredFile, "w") as wf:
+                analyzed_file_lines = rf.readlines()
+                rating_list = []
+                for line in analyzed_file_lines:
+                    ratings = line.replace('compound: ', '')
+                    ratings_split = ratings.split(', ')
+                    rating_list.append(float(ratings_split[0]))
+                avg_rating = sum(rating_list) / len(rating_list)
+                result = self.normalize(sourceLowerBound=-1, sourceUpperBound=1, destinationLowerBound=1, destinationUpperBound=5, sourceValue=avg_rating)
+
+                to_write = "average rating = {0} range:(-1, 1), normalized rating = {1} range:(1, 5)".format(avg_rating, result)
+                wf.write(to_write)
+
+                return result
